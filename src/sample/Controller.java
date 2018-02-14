908b9e921501dbb14210bdabf08bbd5426a31add
bhanuvikas <f20160106@hyderabad.bits-pilani.ac.in>
2018-02-14 17:28:30 +0530

    added file browser code

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private Button browseBtn;
+    @FXML
+    private Button countLinesBtn;
+    @FXML
+    private TextField addressField;
+
+    public void browseBtnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if(selectedFile != null) {
+            addressField.setText(selectedFile.getAbsolutePath());
+        } else {
+            System.out.println("file is not valid");
+        }
+    }
 }
